9132f6a01283d7497d94d5459a97a5f32ad36785
HBX-hbx <1136209374@qq.com>
2023-05-11 17:10:57 +0800

    finish PA2

--- PA2/apsp.cu
+++ PA2/apsp.cu
@@ -3,9 +3,6 @@
 // Brute Force APSP Implementation:
 
 #include "apsp.h"
-#include "cuda_utils.h"
-#include <cassert>
-#include <stdio.h>
 
 /*
 when modify block size, check:
@@ -19,58 +16,116 @@ namespace {
 
 __global__ void kernel_1(int n, int p, int *graph) {
     // coordinate: (j, i), but is the ith row, the jth col, index: i * n + j
-    const int b = 32;
-    __shared__ int s[b][b];
-
-    const auto i_s = threadIdx.y;
-    const auto j_s = threadIdx.x;
+    __shared__ int s[64][64];
+    
+    // in the block or share memory, row: i_s, col: j_s
+    // 0 <= i_s, j_s < b * 2, deal with 4 elements
+    const auto i_s = threadIdx.y << 1;
+    const auto j_s = threadIdx.x << 1;
 
     // the p-th diagonal block
-    const auto i = p * b + i_s;
-    const auto j = p * b + j_s;
+    // upper left: (i_s, j_s)
+    const auto i = p * 64 + i_s;
+    const auto j = p * 64 + j_s;
     const auto index = i * n + j;
 
-    const bool in_bound = i < n && j < n;
+    const bool in_bound_ul = i < n && j < n;
+    const bool in_bound_ur = i < n && j + 1 < n;
+    const bool in_bound_ll = i + 1 < n && j < n;
+    const bool in_bound_lr = i + 1 < n && j + 1 < n;
 
     // copy from global memory to share memory
-    int old_dist;
-    if (in_bound) {
-        old_dist = graph[index];
-        s[i_s][j_s] = old_dist;
+    // load to registers: old_dist_*, 2 * 2 blocks
+    int old_dist_ul;
+    if (in_bound_ul) {
+        old_dist_ul = graph[index];
+        s[i_s][j_s] = old_dist_ul;
     } else {
-        old_dist = 66666666;
-        s[i_s][j_s] = old_dist;
+        old_dist_ul = 66666666;
+        s[i_s][j_s] = old_dist_ul;
+    }
+
+    int old_dist_ur;
+    if (in_bound_ur) {
+        old_dist_ur = graph[index + 1];
+        s[i_s][j_s + 1] = old_dist_ur;
+    } else {
+        old_dist_ur = 66666666;
+        s[i_s][j_s + 1] = old_dist_ur;
+    }
+
+    int old_dist_ll;
+    if (in_bound_ll) {
+        old_dist_ll = graph[index + n];
+        s[i_s + 1][j_s] = old_dist_ll;
+    } else {
+        old_dist_ll = 66666666;
+        s[i_s + 1][j_s] = old_dist_ll;
+    }
+
+    int old_dist_lr;
+    if (in_bound_lr) {
+        old_dist_lr = graph[index + n + 1];
+        s[i_s + 1][j_s + 1] = old_dist_lr;
+    } else {
+        old_dist_lr = 66666666;
+        s[i_s + 1][j_s + 1] = old_dist_lr;
     }
     __syncthreads();
 
-    int new_dist;
-    for (int k_s = 0; k_s < b; ++k_s) {
-        new_dist = s[i_s][k_s] + s[k_s][j_s];
-        old_dist = min(old_dist, new_dist);
-        s[i_s][j_s] = old_dist;
+    int new_dist_ul, new_dist_ur, new_dist_ll, new_dist_lr;
+    #pragma unroll(64)
+    for (int k_s = 0; k_s < 64; ++k_s) {
+        int tmp1 = s[i_s][k_s];
+        int tmp2 = s[i_s + 1][k_s];
+        int tmp3 = s[k_s][j_s];
+        int tmp4 = s[k_s][j_s + 1];
+
+        new_dist_ul = tmp1 + tmp3;
+        old_dist_ul = min(old_dist_ul, new_dist_ul);
+
+        new_dist_ur = tmp1 + tmp4;
+        old_dist_ur = min(old_dist_ur, new_dist_ur);
+
+        new_dist_ll = tmp2 + tmp3;
+        old_dist_ll = min(old_dist_ll, new_dist_ll);
+
+        new_dist_lr = tmp2 + tmp4;
+        old_dist_lr = min(old_dist_lr, new_dist_lr);
     }
 
-    if (in_bound) {
-        graph[index] = s[i_s][j_s];
+    if (in_bound_ul) {
+        graph[index] = old_dist_ul;
+    }
+    if (in_bound_ur) {
+        graph[index + 1] = old_dist_ur;
+    }
+    if (in_bound_ll) {
+        graph[index + n] = old_dist_ll;
+    }
+    if (in_bound_lr) {
+        graph[index + n + 1] = old_dist_lr;
     }
 }
 
 __global__ void kernel_2(int n, int p, int *graph) {
-    const int b = 32;
     // coordinate: (j, i), but is the ith row, the jth col, index: i * n + j
-    __shared__ int s_cross[b][b];  // cross block
-    __shared__ int s_center[b][b]; // center block
+    __shared__ int s_cross[64][64];  // cross block
+    __shared__ int s_center[64][64]; // center block
 
-    // share memory index
-    const auto i_s = threadIdx.y;
-    const auto j_s = threadIdx.x;
+    // in the block or share memory, row: i_s, col: j_s
+    // 0 <= i_s, j_s < b * 2
+    const auto i_s = threadIdx.y << 1;
+    const auto j_s = threadIdx.x << 1;
 
     // blockIdx: (x, y) 0 <= x < max_steps, 0 <= y < 2
     // blockIdx.y == 0: row
     // blockIdx.y == 1: col
+
     // the p-th diagonal block
-    auto ip = p * b + i_s;
-    auto jp = p * b + j_s;
+    // upper left: (i_s, j_s)
+    auto ip = p * 64 + i_s;
+    auto jp = p * 64 + j_s;
     const auto indexp = ip * n + jp;
 
     // copy from global memory to share memory
@@ -79,66 +134,153 @@ __global__ void kernel_2(int n, int p, int *graph) {
     } else {
         s_center[i_s][j_s] = 66666666;
     }
-    __syncthreads();
+
+    if (ip < n && jp + 1 < n) {
+        s_center[i_s][j_s + 1] = graph[indexp + 1];
+    } else {
+        s_center[i_s][j_s + 1] = 66666666;
+    }
+
+    if (ip + 1 < n && jp < n) {
+        s_center[i_s + 1][j_s] = graph[indexp + n];
+    } else {
+        s_center[i_s + 1][j_s] = 66666666;
+    }
+
+    if (ip + 1 < n && jp + 1 < n) {
+        s_center[i_s + 1][j_s + 1] = graph[indexp + n + 1];
+    } else {
+        s_center[i_s + 1][j_s + 1] = 66666666;
+    }
 
     auto i = ip, j = jp;
+
     if (blockIdx.y == 0) {
-        i = blockIdx.x * b + i_s;
+        i = blockIdx.x * 64 + i_s;
     } else {
-        j = blockIdx.x * b + j_s;
+        j = blockIdx.x * 64 + j_s;
     }
-    const bool in_bound = i < n && j < n;
+
     const auto index = i * n + j;
-    int old_dist;
+
+    const bool in_bound_ul = i < n && j < n;
+    const bool in_bound_ur = i < n && j + 1 < n;
+    const bool in_bound_ll = i + 1 < n && j < n;
+    const bool in_bound_lr = i + 1 < n && j + 1 < n;
 
     // copy from global memory to share memory
-    if (in_bound) {
-        old_dist = graph[index];
-        s_cross[i_s][j_s] = old_dist;
+    int old_dist_ul;
+    if (in_bound_ul) {
+        old_dist_ul = graph[index];
+        s_cross[i_s][j_s] = old_dist_ul;
     } else {
-        old_dist = 66666666;
-        s_cross[i_s][j_s] = old_dist;
+        old_dist_ul = 66666666;
+        s_cross[i_s][j_s] = old_dist_ul;
+    }
+
+    int old_dist_ur;
+    if (in_bound_ur) {
+        old_dist_ur = graph[index + 1];
+        s_cross[i_s][j_s + 1] = old_dist_ur;
+    } else {
+        old_dist_ur = 66666666;
+        s_cross[i_s][j_s + 1] = old_dist_ur;
+    }
+
+    int old_dist_ll;
+    if (in_bound_ll) {
+        old_dist_ll = graph[index + n];
+        s_cross[i_s + 1][j_s] = old_dist_ll;
+    } else {
+        old_dist_ll = 66666666;
+        s_cross[i_s + 1][j_s] = old_dist_ll;
+    }
+
+    int old_dist_lr;
+    if (in_bound_lr) {
+        old_dist_lr = graph[index + n + 1];
+        s_cross[i_s + 1][j_s + 1] = old_dist_lr;
+    } else {
+        old_dist_lr = 66666666;
+        s_cross[i_s + 1][j_s + 1] = old_dist_lr;
     }
     __syncthreads();
 
-    int new_dist;
+    int new_dist_ul, new_dist_ur, new_dist_ll, new_dist_lr;
     if (blockIdx.y == 0) { // row
-        for (int k_s = 0; k_s < b; ++k_s) {
-            new_dist = s_cross[i_s][k_s] + s_center[k_s][j_s];
-            old_dist = min(old_dist, new_dist);
-            s_cross[i_s][j_s] = old_dist;
+        #pragma unroll(64)
+        for (int k_s = 0; k_s < 64; ++k_s) {
+            int tmp1 = s_cross[i_s][k_s];
+            int tmp2 = s_cross[i_s + 1][k_s];
+            int tmp3 = s_center[k_s][j_s];
+            int tmp4 = s_center[k_s][j_s + 1];
+
+            new_dist_ul = tmp1 + tmp3;
+            old_dist_ul = min(old_dist_ul, new_dist_ul);
+
+            new_dist_ur = tmp1 + tmp4;
+            old_dist_ur = min(old_dist_ur, new_dist_ur);
+
+            new_dist_ll = tmp2 + tmp3;
+            old_dist_ll = min(old_dist_ll, new_dist_ll);
+
+            new_dist_lr = tmp2 + tmp4;
+            old_dist_lr = min(old_dist_lr, new_dist_lr);
         }
     } else { // col
-        for (int k_s = 0; k_s < b; ++k_s) {
-            new_dist = s_center[i_s][k_s] + s_cross[k_s][j_s];
-            old_dist = min(old_dist, new_dist);
-            s_cross[i_s][j_s] = old_dist;
+        #pragma unroll(64)
+        for (int k_s = 0; k_s < 64; ++k_s) {
+            int tmp1 = s_center[i_s][k_s];
+            int tmp2 = s_center[i_s + 1][k_s];
+            int tmp3 = s_cross[k_s][j_s];
+            int tmp4 = s_cross[k_s][j_s + 1];
+
+            new_dist_ul = tmp1 + tmp3;
+            old_dist_ul = min(old_dist_ul, new_dist_ul);
+
+            new_dist_ur = tmp1 + tmp4;
+            old_dist_ur = min(old_dist_ur, new_dist_ur);
+
+            new_dist_ll = tmp2 + tmp3;
+            old_dist_ll = min(old_dist_ll, new_dist_ll);
+
+            new_dist_lr = tmp2 + tmp4;
+            old_dist_lr = min(old_dist_lr, new_dist_lr);
         }
     }
-    if (in_bound) {
-        graph[index] = s_cross[i_s][j_s];
+    if (in_bound_ul) {
+        graph[index] = old_dist_ul;
+    }
+    if (in_bound_ur) {
+        graph[index + 1] = old_dist_ur;
+    }
+    if (in_bound_ll) {
+        graph[index + n] = old_dist_ll;
+    }
+    if (in_bound_lr) {
+        graph[index + n + 1] = old_dist_lr;
     }
 }
 
 __global__ void kernel_3(int n, int p, int *graph) {
-    const int b = 32;
     // coordinate: (j, i), but is the ith row, the jth col, index: i * n + j
-    __shared__ int s_cross_row[b][b];  // cross row block
-    __shared__ int s_cross_col[b][b];  // cross col block
+    __shared__ int s_cross_row[64][64];  // cross row block
+    __shared__ int s_cross_col[64][64];  // cross col block
 
     // in the block or share memory, row: i_s, col: j_s
-    // 0 <= i_s, j_s < b
-    const auto i_s = threadIdx.y;
-    const auto j_s = threadIdx.x;
+    // 0 <= i_s, j_s < b * 2
+    const auto i_s = threadIdx.y << 1;
+    const auto j_s = threadIdx.x << 1;
 
-    // center block
-    const auto ip = p * b + i_s;
-    const auto jp = p * b + j_s;
+    // the p-th diagonal block
+    // upper left: (i_s, j_s)
+    const auto ip = p * 64 + i_s;
+    const auto jp = p * 64 + j_s;
 
     // in the whole graph(n * n), row: i, col: j, index: i * n + j
     // 0 <= i, j < max_steps * b, may exceed n
-    const auto i = blockIdx.y * b + i_s;
-    const auto j = blockIdx.x * b + j_s;
+    const auto i = blockIdx.y * 64 + i_s;
+    const auto j = blockIdx.x * 64 + j_s;
     const auto index = i * n + j;
 
     // (ip, j) and (ip, jp) are in the same row
@@ -147,22 +289,108 @@ __global__ void kernel_3(int n, int p, int *graph) {
     } else {
         s_cross_row[i_s][j_s] = 66666666;
     }
+
+    if (ip < n && j + 1 < n) {
+        s_cross_row[i_s][j_s + 1] = graph[ip * n + j + 1];
+    } else {
+        s_cross_row[i_s][j_s + 1] = 66666666;
+    }
+
+    if (ip + 1 < n && j < n) {
+        s_cross_row[i_s + 1][j_s] = graph[ip * n + j + n];
+    } else {
+        s_cross_row[i_s + 1][j_s] = 66666666;
+    }
+
+    if (ip + 1 < n && j + 1 < n) {
+        s_cross_row[i_s + 1][j_s + 1] = graph[ip * n + j + n + 1];
+    } else {
+        s_cross_row[i_s + 1][j_s + 1] = 66666666;
+    }
+
     // (i, jp) and (ip, jp) are in the same col
     if (i < n && jp < n) {
         s_cross_col[i_s][j_s] = graph[i * n + jp];
     } else {
         s_cross_col[i_s][j_s] = 66666666;
     }
+
+    if (i < n && jp + 1 < n) {
+        s_cross_col[i_s][j_s + 1] = graph[i * n + jp + 1];
+    } else {
+        s_cross_col[i_s][j_s + 1] = 66666666;
+    }
+
+    if (i + 1 < n && jp < n) {
+        s_cross_col[i_s + 1][j_s] = graph[i * n + jp + n];
+    } else {
+        s_cross_col[i_s + 1][j_s] = 66666666;
+    }
+
+    if (i + 1 < n && jp + 1 < n) {
+        s_cross_col[i_s + 1][j_s + 1] = graph[i * n + jp + n + 1];
+    } else {
+        s_cross_col[i_s + 1][j_s + 1] = 66666666;
+    }
     __syncthreads();
 
-    int old_dist = graph[index];
-    int new_dist;
-    for (int k_s = 0; k_s < b; ++k_s) {
-        new_dist = s_cross_col[i_s][k_s] + s_cross_row[k_s][j_s];
-        old_dist = min(old_dist, new_dist);
+    const bool in_bound_ul = i < n && j < n;
+    const bool in_bound_ur = i < n && j + 1 < n;
+    const bool in_bound_ll = i + 1 < n && j < n;
+    const bool in_bound_lr = i + 1 < n && j + 1 < n;
+
+    int old_dist_ul, old_dist_ur, old_dist_ll, old_dist_lr;
+    if (in_bound_ul) {
+        old_dist_ul = graph[index];
     }
-    if (i < n && j < n) {
-        graph[index] = old_dist;
+
+    if (in_bound_ur) {
+        old_dist_ur = graph[index + 1];
+    }
+
+    if (in_bound_ll) {
+        old_dist_ll = graph[index + n];
+    }
+
+    if (in_bound_lr) {
+        old_dist_lr = graph[index + n + 1];
+    }
+
+    int new_dist_ul, new_dist_ur, new_dist_ll, new_dist_lr;
+    #pragma unroll(64)
+    for (int k_s = 0; k_s < 64; ++k_s) {
+        int tmp1 = s_cross_col[i_s][k_s];
+        int tmp2 = s_cross_col[i_s + 1][k_s];
+        int tmp3 = s_cross_row[k_s][j_s];
+        int tmp4 = s_cross_row[k_s][j_s + 1];
+
+        new_dist_ul = tmp1 + tmp3;
+        old_dist_ul = min(old_dist_ul, new_dist_ul);
+
+        new_dist_ur = tmp1 + tmp4;
+        old_dist_ur = min(old_dist_ur, new_dist_ur);
+
+        new_dist_ll = tmp2 + tmp3;
+        old_dist_ll = min(old_dist_ll, new_dist_ll);
+
+        new_dist_lr = tmp2 + tmp4;
+        old_dist_lr = min(old_dist_lr, new_dist_lr);
+    }
+
+    if (in_bound_ul) {
+        graph[index] = old_dist_ul;
+    }
+
+    if (in_bound_ur) {
+        graph[index + 1] = old_dist_ur;
+    }
+
+    if (in_bound_ll) {
+        graph[index + n] = old_dist_ll;
+    }
+
+    if (in_bound_lr) {
+        graph[index + n + 1] = old_dist_lr;
     }
 }
 
@@ -171,56 +399,17 @@ __global__ void kernel_3(int n, int p, int *graph) {
 void apsp(int n, /* device */ int *graph) {
     // max shared_memory: 48KB, max number of int: 12000
     // blk_size b: 32 * 32
-    // max_steps: (n - 1) / 32 + 1
-    constexpr int b = 32;
-    int max_steps = (n - 1) / b + 1;
-    dim3 thr(b, b);
+    // max_steps: (n - 1) / 64 + 1
+    int max_steps = (n - 1) / 64 + 1;
+    dim3 thr(32, 32);
     dim3 blk1(1, 1);
     dim3 blk2(max_steps, 2);
     dim3 blk3(max_steps, max_steps);
 
     for (int p = 0; p < max_steps; ++p){
         kernel_1<<<blk1, thr>>>(n, p, graph);
-        // // test:
-        // CHK_CUDA_ERR(cudaDeviceSynchronize());
-        // // Check for kernel launch errors
-        // cudaError_t error = cudaGetLastError();
-        // if (error != cudaSuccess) {
-        //     printf("Kernel_1 launch failed with error: %s\n", cudaGetErrorString(error));
-        //     exit(1);
-        // }
-
         kernel_2<<<blk2, thr>>>(n, p, graph);
-
-        // kernel_2_row<<<blk2_1, thr>>>(n, p, graph);
-        // // test:
-        // CHK_CUDA_ERR(cudaDeviceSynchronize());
-        // // Check for kernel launch errors
-        // error = cudaGetLastError();
-        // if (error != cudaSuccess) {
-        //     printf("Kernel_2_row launch failed with error: %s\n", cudaGetErrorString(error));
-        //     exit(1);
-        // }
-
-        // kernel_2_col<<<blk2_2, thr>>>(n, p, graph);
-        // // test:
-        // CHK_CUDA_ERR(cudaDeviceSynchronize());
-        // // Check for kernel launch errors
-        // error = cudaGetLastError();
-        // if (error != cudaSuccess) {
-        //     printf("Kernel_2_col launch failed with error: %s\n", cudaGetErrorString(error));
-        //     exit(1);
-        // }
-
         kernel_3<<<blk3, thr>>>(n, p, graph);
-        // // test:
-        // CHK_CUDA_ERR(cudaDeviceSynchronize());
-        // // Check for kernel launch errors
-        // error = cudaGetLastError();
-        // if (error != cudaSuccess) {
-        //     printf("Kernel_3 launch failed with error: %s\n", cudaGetErrorString(error));
-        //     exit(1);
-        // }
     }
 
 }
